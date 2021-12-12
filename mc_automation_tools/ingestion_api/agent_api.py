dd51def81c9d6433bc42dc41fd33d561c880bdf4
ronenk1 <ronenk1@rnd-hub.com>
2021-12-12 10:34:58 +0200

    fix(automation-kit): agent api + change to class and implement environment

--- mc_automation_tools/ingestion_api/agent_api.py
+++ mc_automation_tools/ingestion_api/agent_api.py
@@ -3,44 +3,64 @@
 This module wrapping agent api's restful functionality
 """
 
-from mc_automation_tools.configuration import config
 from mc_automation_tools import common, base_requests
 
 
-def post_manual_trigger(source_directory):
-    """
-    This method triggering ingestion process by manual method (from given valid directory
-    """
-    # todo - add directory inner validator
-    body = {'sourceDirectory': source_directory}
-    url = common.combine_url(config.INGESTION_AGENT_URL, config.INGESTION_MANUAL_TRIGGER)
-    resp = base_requests.send_post_request(url, body)
-    return resp
+class DiscreteAgentApi:
+    __trigger = 'trigger'
+    __status = 'status'
+    __start = 'start'
+    __stop = 'stop'
 
+    def __init__(self, end_point_url):
+        self.__end_point_url = end_point_url
 
-def get_watching_statuses():
-    """
-    This method return bool -> true if watcher is on, false if watcher
-    """
-    url = common.combine_url(config.INGESTION_AGENT_URL, config.INGESTION_WATCHER_STATUS)
-    resp = base_requests.send_get_request(url)
-    return resp
+    @property
+    def get_class_params(self):
+        params = {
+            'trigger': self.__trigger,
+            'status': self.__status,
+            'start': self.__start,
+            'stop': self.__stop,
+            "API url's": {'manual_trigger': common.combine_url(self.__end_point_url, self.__trigger),
+                          'watch_status': common.combine_url(self.__end_point_url, self.__status),
+                          'start_watch': common.combine_url(self.__end_point_url, self.__status, self.__start),
+                          'stop_watch': common.combine_url(self.__end_point_url, self.__status, self.__stop)
 
+                          }
+        }
+        return params
 
-def post_start_watch():
-    """
-    This method change watcher status to true and return -> "watching": true
-    """
-    url = common.combine_url(config.INGESTION_AGENT_URL, config.INGESTION_WATCHER_STATUS,
-                             config.INGESTION_START_WATCHER)
-    resp = base_requests.send_post_request(url)
-    return resp
+    # ===============================================Agent api's========================================================
+    def post_manual_trigger(self, source_directory):
+        """
+        This method triggering ingestion process by manual method (from given valid directory
+        """
+        body = {'sourceDirectory': source_directory}
+        url = common.combine_url(self.__end_point_url, self.__trigger)
+        resp = base_requests.send_post_request(url, body)
+        return resp
 
+    def get_watching_statuses(self):
+        """
+        This method return bool -> true if watcher is on, false if watcher
+        """
+        url = common.combine_url(self.__end_point_url, self.__status)
+        resp = base_requests.send_get_request(url)
+        return resp
 
-def post_stop_watch():
-    """
-    This method change watcher status to true and return -> "watching": false
-    """
-    url = common.combine_url(config.INGESTION_AGENT_URL, config.INGESTION_WATCHER_STATUS, config.INGESTION_STOP_WATCHER)
-    resp = base_requests.send_post_request(url)
-    return resp
+    def post_start_watch(self):
+        """
+        This method change watcher status to true and return -> "watching": true
+        """
+        url = common.combine_url(self.__end_point_url, self.__status, self.__start)
+        resp = base_requests.send_post_request(url)
+        return resp
+
+    def post_stop_watch(self):
+        """
+        This method change watcher status to true and return -> "watching": false
+        """
+        url = common.combine_url(self.__end_point_url, self.__status, self.__stop)
+        resp = base_requests.send_post_request(url)
+        return resp
