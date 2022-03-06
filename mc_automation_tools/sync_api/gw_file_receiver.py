64e70654f155015734b2a36d4ba1b825f6ccd0f1
ronenk1 <ronenk1@rnd-hub.com>
2022-03-06 16:58:46 +0200

    chore(automation-kit): merge with master

--- mc_automation_tools/sync_api/gw_file_receiver.py
+++ mc_automation_tools/sync_api/gw_file_receiver.py
@@ -2,7 +2,6 @@
 This module will wrap and provide api's functionality of layer spec api of sync services
 """
 
-import json
 import logging
 from mc_automation_tools.configuration import config
 from mc_automation_tools import base_requests, common
@@ -52,16 +51,6 @@ class FileReceiver:
         return status_code, content_dict
 
 
-# todo -> samples for sync automation integration -> should be removed after integration
-# fr = FileReceiver('https://gw-file-receiver-qa-sync-gw-file-receiver-route-raster-qa.apps.v0h0bdx6.eastus.aroapp.io/')
-# import glob
-# layer_path = '/tmp/sync_test_data/test_receive_2022_02_28T10_32_38Z'
-# tiles_path_list = [f for f in glob.glob(f'{layer_path}/**/*.png', recursive=True)]
-# my_tile = tiles_path_list[0]
-# toc = [f for f in glob.glob(f'{layer_path}/**/*.json', recursive=True)][0]
-# fr.send_to_file_receiver('test_receive_2022_02_28T10_32_38Z/4.0/OrthophotoHistory/12/4894/2772.png', open(my_tile, "rb").read())
-# # fr.send_to_file_receiver('test_receive_2022_02_28T10_32_38Z/4.0/OrthophotoHistory/toc.json', json.dumps(open(toc, "rb").read()))
-# fr.send_to_file_receiver('test_receive_2022_02_28T10_32_38Z/4.0/OrthophotoHistory/toc.json', open(toc,'rb').read())
 
 
 
